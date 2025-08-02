c83a021e57268015ed5d10fcc1731160292061c1
Che <johnccreations21@gmail.com>
2025-08-02 10:10:39 -0400

    first update

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -4,6 +4,8 @@ import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 
 const NewItems = () => {
+  
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
